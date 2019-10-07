7338a53ae969dd11c65d4342701cf2f41d70c542
amxra <amiradediran@gmail.com>
2019-10-07 18:28:25 +0100

    Import ProductContext into app.js, create state and refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,16 +18,14 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
+				component = {Products}
 					/>
 				)}
 			/>
@@ -35,6 +34,7 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
